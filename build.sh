49948d047b2f8fac15e2329f4310d40745453878
Denis Davydov <davydov.ds@nsd.ru>
2021-11-21 21:12:01 +0300

    # Дз 25

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 export USER_NAME=drinkingteam
 
-for i in ui post-py comment; do cd src/$i; bash docker_build.sh; cd -; done
+cd ./src/ui && bash docker_build.sh && docker push $USER_NAME/ui
+cd ../post-py && bash docker_build.sh && docker push $USER_NAME/post
+cd ../comment && bash docker_build.sh && docker push $USER_NAME/comment
+
+#for i in ui post-py comment; do cd src/$i; bash docker_build.sh; cd -; done
--- monitoring/create-vm.sh
+++ monitoring/create-vm.sh
@@ -5,4 +5,5 @@ yc compute instance create \
   --zone ru-central1-a \
   --network-interface subnet-id=e9boarnj7cna0dhq13l5,nat-ip-version=ipv4 \
   --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1804-lts,size=15 \
+  --memory 4 \
   --ssh-key ~/.ssh/appuser.pub
